d6d1fedecb9c204de6da36e677cff63750b4efd6
Kayky Fidelis <kayky.fid@gmail.com>
2025-09-28 22:30:35 -0300

    Arrumei a nav

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 const second = () => {
   return (
-     <section className=' w-full h-screen bg-black flex items-center justify-center overflow-hidden '>
+     <section className=' w-full h-screen bg-white flex items-center justify-center overflow-hidden '>
+        
         
      </section>
   )
