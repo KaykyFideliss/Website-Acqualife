619ade96e4580da4f15dfae5757758fbe9f36b4f
Kayky Fidelis <kayky.fid@gmail.com>
2025-09-29 22:32:40 -0300

    Estou fazendo a section da frase motivadora

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
-const second = () => {
+const Second = () => {
   return (
-     <section className=' w-full h-screen bg-white flex items-center justify-center overflow-hidden '>
-        
-        
-     </section>
+<section className="h-screen w-full flex items-center justify-center bg-white px-6">
+  <div className="text-center text-blue-500 text-4xl md:text-7xl font-bold font-zalando leading-snug">
+    Acredite no seu potencial,<br />
+    <span className="text-gray-800">você é capaz de conquistar mais do que imagina.</span>
+  </div>
+</section>
+
   )
 }
 
-export default second
+export default Second
