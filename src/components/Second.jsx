53232cb1ee28ee052a74cf832aa4bfbb1bfafec9
Kayky Fidelis <kayky.fid@gmail.com>
2025-10-01 22:19:10 -0300

    Dia nada produtivo estou meendo na aba de download

--- src/components/Second.jsx
+++ src/components/Second.jsx
@@ -4,7 +4,7 @@ const Second = () => {
   return (
 <section className="h-screen w-full flex items-center justify-center bg-white px-6">
   <div className="text-center text-gray-900 text-4xl md:text-7xl font-bold font-zalando leading-snug">
-    Transforme <span className="text-blue-500">cada gotade chuva</span>  em uma <span className="text-blue-500">ação sustentável.</span><br />
+    Transforme <span className="text-blue-500">cada gota de chuva</span>  em uma <span className="text-blue-500">ação sustentável.</span><br />
   </div>
 </section>  
 
---
+++ src/components/WhoTouse.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const WhoTouse = () => {
+  return (
+   
+        <section className='h-screen w-full  px-6'>
+
+ {/* Título */}
+      <div className="flex flex-col justify-center items-center pt-20 mb-10">
+        <h1 className="text-4xl md:text-7xl font-bold font-zalando text-[#0D6DFF] mb-4">
+            ENTENDA CADA TANQUE
+        </h1>
+      </div> 
+
+        </section>
+      
+
+  )
+}
+
+export default WhoTouse
